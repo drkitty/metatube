0ab6dd298095a99431b43bfdf874797f0336cb96
drkitty <bumblingkitty@gmail.com>
2014-05-17 16:32:28 -0700

    Split up normal and special playlist getters

--- tube.py
+++ tube.py
@@ -3,57 +3,58 @@ from __future__ import unicode_literals
 import dateutil.parser
 
 
-def get_user_playlists(client, username=None, likes=False, favorites=False,
-                       uploads=False):
+def get_normal_playlists(client, channel_id=None):
     playlists = []
 
     def process_playlist(item):
         snippet = item['snippet']
-        p = {
+        playlists.append({
             'id': item['id'],
             'title': snippet['title'],
             'description': snippet['description'],
-        }
-        playlists.append(p)
+        })
 
-    if username is None:
-        # normal playlists
+    client.get('/playlists', {
+        'part': 'snippet',
+        'mine': 'true',
+    }, process_playlist)
+
+    return playlists
+
+def get_special_playlists(client, channel_id=None, names=()):
+    playlists = []
+
+    def process_playlist(item):
+        snippet = item['snippet']
+        playlists.append({
+            'id': item['id'],
+            'title': snippet['title'],
+            'description': snippet['description'],
+        })
+
+    def process_channel(item):
+        special_playlists = item['contentDetails']['relatedPlaylists']
+        selected_playlist_ids = []
+
+        print special_playlists
+        for name in names:
+            if name in special_playlists:
+                selected_playlist_ids.append(special_playlists[name])
 
         client.get('/playlists', {
             'part': 'snippet',
-            'mine': 'true',
+            'id': ','.join(selected_playlist_ids),
         }, process_playlist)
 
-    if likes or favorites or uploads:
-        # special playlists
+    params = {
+        'part': 'contentDetails',
+    }
+    if channel_id is None:
+        params['mine'] = 'true'
+    else:
+        params['id'] = channel_id
 
-        def process_channel(item):
-            special_playlists = item['contentDetails']['relatedPlaylists']
-
-            title_list = []
-            if likes:
-                title_list.append('likes')
-            if favorites:
-                title_list.append('favorites')
-            if uploads:
-                title_list.append('uploads')
-
-            client.get('/playlists', {
-                'part': 'snippet',
-                'id': ','.join(special_playlists[title]
-                               for title in title_list),
-            }, process_playlist)
-
-        params = {
-            'part': 'contentDetails',
-        }
-
-        if username is None:
-            params['mine'] = 'true'
-        else:
-            params['forUsername'] = username
-
-        client.get('/channels', params, process_channel)
+    client.get('/channels', params, process_channel)
 
     return playlists
 

77ee43c4acb58b1e3329e5d010c3c3178ccf5ea1
drkitty <bumblingkitty@gmail.com>
2014-05-17 16:05:06 -0700

    Fix some stuff

--- tube.py
+++ tube.py
@@ -3,10 +3,9 @@ from __future__ import unicode_literals
 import dateutil.parser
 
 
-def get_my_playlists(client):
+def get_user_playlists(client, username=None, likes=False, favorites=False,
+                       uploads=False):
     playlists = []
-
-    # normal playlists
 
     def process_playlist(item):
         snippet = item['snippet']
@@ -17,28 +16,47 @@ def get_my_playlists(client):
         }
         playlists.append(p)
 
-    client.get('/playlists', {
-        'part': 'snippet',
-        'mine': 'true',
-    }, process_playlist)
-
-    # special playlists
-
-    def process_channel(item):
-        special_playlists = item['contentDetails']['relatedPlaylists']
+    if username is None:
+        # normal playlists
 
         client.get('/playlists', {
             'part': 'snippet',
-            'id': ','.join(special_playlists[title]
-                           for title in ('favorites', 'likes')),
+            'mine': 'true',
         }, process_playlist)
 
-    client.get('/channels', {
-        'part': 'contentDetails',
-        'mine': 'true',
-    }, process_channel)
+    if likes or favorites or uploads:
+        # special playlists
+
+        def process_channel(item):
+            special_playlists = item['contentDetails']['relatedPlaylists']
+
+            title_list = []
+            if likes:
+                title_list.append('likes')
+            if favorites:
+                title_list.append('favorites')
+            if uploads:
+                title_list.append('uploads')
+
+            client.get('/playlists', {
+                'part': 'snippet',
+                'id': ','.join(special_playlists[title]
+                               for title in title_list),
+            }, process_playlist)
+
+        params = {
+            'part': 'contentDetails',
+        }
+
+        if username is None:
+            params['mine'] = 'true'
+        else:
+            params['forUsername'] = username
+
+        client.get('/channels', params, process_channel)
 
     return playlists
+
 
 def get_playlist_videos(client, playlist_id):
     videos = []
